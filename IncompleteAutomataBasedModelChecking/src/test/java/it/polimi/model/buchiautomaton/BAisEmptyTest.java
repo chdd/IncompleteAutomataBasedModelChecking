7c5f6e6c3fa75e23713f7b14321755a8226dab27
claudiomenghi <cla.menghi@gmail.com>
2014-09-10 21:06:24 +0200

    BuchiAutomatonIsEmptyTest renamed in BAisEmptyTest

---
+++ IncompleteAutomataBasedModelChecking/src/test/java/it/polimi/model/buchiautomaton/BAisEmptyTest.java
@@ -0,0 +1,49 @@
+package it.polimi.model.buchiautomaton;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import it.polimi.model.BuchiAutomaton;
+import it.polimi.model.State;
+import it.polimi.model.Transition;
+
+import org.junit.Test;
+
+/**
+ * tests the is Empty method
+ * @author claudiomenghi
+ *
+ */
+public class BAisEmptyTest {
+
+	// Creates a new test where there are three states in sequence and a self loop on the state s3
+	@Test
+	public void isEmptyTest1() {
+		// creates the states
+		State s1=new State("s1");
+		State s2=new State("s2");
+		State s3=new State("s3");
+		// creates the alphabet
+		Set<String> alphabet=new HashSet<String>();
+		alphabet.add("a");
+		//creates the Buchi automaton
+		BuchiAutomaton<State, Transition<State>> a=new BuchiAutomaton<State, Transition<State>>(alphabet);
+		// add the states into the automaton
+		a.addState(s1);
+		a.addState(s2);
+		a.addState(s3);
+		// add the initial state
+		a.addInitialState(s1);
+		// add the accepting state
+		a.addAcceptState(s3);
+		// add the transitions
+		a.addTransition(s1, new Transition<State>("a", s2));
+		a.addTransition(s2, new Transition<State>("a", s3));
+		a.addTransition(s3, new Transition<State>("a", s3));
+		// assert that the automaton is not empty
+		assertFalse(a.isEmpty());
+	}
+
+}
