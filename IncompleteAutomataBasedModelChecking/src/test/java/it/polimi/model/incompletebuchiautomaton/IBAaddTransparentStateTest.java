5db2bf09d1d45030454127fe7c291e3ad0f464a9
claudiomenghi <cla.menghi@gmail.com>
2014-09-10 16:54:37 +0200

    IBAaddTransparenStateTest added

---
+++ IncompleteAutomataBasedModelChecking/src/test/java/it/polimi/model/incompletebuchiautomaton/IBAaddTransparentStateTest.java
@@ -0,0 +1,49 @@
+package it.polimi.model.incompletebuchiautomaton;
+
+import static org.junit.Assert.*;
+import it.polimi.model.IncompleteBuchiAutomaton;
+import it.polimi.model.State;
+import it.polimi.model.Transition;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+/**
+ * this class test the behavior of the method addTransparentState
+ * @author claudiomenghi
+ */
+public class IBAaddTransparentStateTest {
+
+	/** 
+	 * tests the behavior of the method when s2 is a state
+	 */
+	@Test
+	public void test1() {
+		Set<String> alphabet=new HashSet<String>();
+		IncompleteBuchiAutomaton<State, Transition<State>> iba=new IncompleteBuchiAutomaton<State, Transition<State>>(alphabet);
+		State s1=new State("a");
+		iba.addTransparentState(s1);
+	
+		assertTrue(iba.isTransparent(s1));
+		assertTrue(iba.isContained(s1));
+		assertFalse(iba.isAccept(s1));
+		assertFalse(iba.isInitial(s1));
+		
+		assertTrue(iba.getStates().contains(s1));
+		assertTrue(iba.getTransparentStates().contains(s1));
+		assertFalse(iba.getAcceptStates().contains(s1));
+		assertFalse(iba.getInitialStates().contains(s1));
+	}
+	/**
+	 * tests the behavior of the method when the added state is transparent 
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void test2() {
+		Set<String> alphabet=new HashSet<String>();
+		IncompleteBuchiAutomaton<State, Transition<State>> iba=new IncompleteBuchiAutomaton<State, Transition<State>>(alphabet);
+		iba.addTransparentState(null);	
+	}
+
+}
