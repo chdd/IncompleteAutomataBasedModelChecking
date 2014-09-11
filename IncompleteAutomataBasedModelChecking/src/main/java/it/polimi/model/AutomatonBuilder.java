578cc70c8c84e4a519c2212dc41e742286de2f04
claudiomenghi <cla.menghi@gmail.com>
2014-09-11 08:52:33 +0200

    AutomatonBuilder, javadoc updated

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/model/AutomatonBuilder.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/model/AutomatonBuilder.java
@@ -20,6 +20,16 @@ import org.xml.sax.SAXException;
 public class AutomatonBuilder<S extends State, T extends Transition<S>, A extends BuchiAutomaton<S, T>> {
 	
 
+	/**
+	 * loads the automaton from a file which contains the XML representation of the automaton
+	 * @param type is the type of the automaton to be loaded
+	 * @param filePath is the path of the file from which the automaton must be loaded
+	 * @return the loaded automaton of type type
+	 * @throws JAXBException
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 */
 	public A loadAutomaton(Class<? extends BuchiAutomaton> type, String filePath) throws JAXBException, SAXException, IOException, ParserConfigurationException{
 		File file = new File(filePath);
 		JAXBContext jaxbContext = JAXBContext.newInstance(type);
@@ -28,6 +38,13 @@ public class AutomatonBuilder<S extends State, T extends Transition<S>, A extend
 		return (A) jaxbUnmarshaller.unmarshal(file);
 	}
 	
+	/**
+	 * loads the automaton from a String which contains the XML representation of the automaton
+	 * @param type is the type of the automaton to be loaded
+	 * @param automatonText contains the XML description of the automaton
+	 * @return the loaded automaton of the specified type
+	 * @throws JAXBException
+	 */
 	public A loadAutomatonFromText(Class<? extends BuchiAutomaton> type,  String automatonText) throws JAXBException{
 		JAXBContext jaxbContext = JAXBContext.newInstance(type);
 		 
