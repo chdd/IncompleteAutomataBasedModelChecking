807d3c7d1db1ac70f1e17dc81c1a52ca1e55d3e9
claudiomenghi <cla.menghi@gmail.com>
2014-09-12 09:30:16 +0200

    Javadoc of the class EpsilonPredicate updated

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/modelchecker/brzozowski/predicates/EpsilonPredicate.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/modelchecker/brzozowski/predicates/EpsilonPredicate.java
@@ -3,111 +3,129 @@ package it.polimi.modelchecker.brzozowski.predicates;
 import it.polimi.model.State;
 
 /**
- * @author Claudio Menghi
- * contains the epsilon constraint
+ * @author claudiomenghi
+ * contains an epsilon Predicate. This predicate is associated with regular transitions of the (I)BA since these transitions
+ * are not relevant in the predicate computation
  */
 public class EpsilonPredicate<S extends State> extends AbstractPredicate<S>{
 
 	/**
-	 * the concatenation of a epsilon constraint is defined as follows:
-	 * -	if a is an empty constraint the empty constraint is returned
-	 * -	the concatenation of an epsilon constraint and the lambda constraint is equal to the epsilon constraint
-	 * -	the concatenation of an epsilon constraint and an epsilon constraint is equal to the epsilon constraint
-	 * -	the concatenation of an epsilon constraint and a predicate is a new and constraint that contains the epsilon constraint
+	 * the concatenation of a epsilon predicate is defined as follows:
+	 * -	if a is an empty predicate the empty predicate is returned
+	 * -	the concatenation of an epsilon predicate and the lambda predicate is equal to the epsilon predicate
+	 * -	the concatenation of an epsilon predicate and an epsilon predicate is equal to the epsilon predicate
+	 * -	the concatenation of an epsilon predicate and a predicate is a new and predicate that contains the epsilon predicate
 	 * 		and the predicate
-	 * -	the concatenation of an epsilon constraint and an and constraint is a new and constraint that contains the epsilon constraint
-	 *		and the original and constraint
-	 * -	the concatenation of an epsilon constraint and an or constraint is a new and constraint that contains the epsilon constraint
-	 * 		and the original or constraint
-	 * @param a: is the constraint to be concatenated
-	 * @return the constraint which is the concatenation of the epsilon constraint and a
-	 * @throws IllegalArgumentException is generated when the constraint to be concatenated is null
+	 * -	the concatenation of an epsilon predicate and an and predicate is a new and predicate that contains the epsilon predicate
+	 *		and the original and predicate
+	 * -	the concatenation of an epsilon predicate and an or predicate is a new and predicate that contains the epsilon predicate
+	 * 		and the original or predicate
+	 * @param a: is the predicate to be concatenated
+	 * @return the predicate which is the concatenation of the epsilon predicate and a
+	 * @throws IllegalArgumentException is generated when the predicate to be concatenated is null
 	 */
 	@Override
 	public AbstractPredicate<S> concatenate(AbstractPredicate<S> a) {
 		if(a==null){
-			throw new IllegalArgumentException("The constraint to be concatenated cannot be null");
+			throw new IllegalArgumentException("The predicate to be concatenated cannot be null");
 		}
-		// if a is an empty constraint the empty constraint is returned
+		// if a is an empty predicate the empty predicate is returned
 		if(a instanceof EmptyPredicate){
 			return a;
 		}
-		// the concatenation of an epsilon constraint and the lambda constraint is equal to the epsilon constraint
+		// the concatenation of an epsilon predicate and the lambda predicate is equal to the epsilon predicate
 		if(a instanceof LambdaPredicate){
 			return this;
 		}
-		// the concatenation of an epsilon constraint and an epsilon constraint is equal to the epsilon constraint
+		// the concatenation of an epsilon predicate and an epsilon predicate is equal to the epsilon predicate
 		if(a instanceof EpsilonPredicate){
 			return this;
 		}
-		// the concatenation of an epsilon constraint and a predicate is a new and constraint that contains the epsilon constraint and the predicate
+		// the concatenation of an epsilon predicate and a predicate is a new and predicate that contains the epsilon predicate and the predicate
 		if(a instanceof Predicate){
 			return new AndPredicate<S>(this, a);
 		}
-		// the concatenation of an epsilon constraint and an and constraint is a new and constraint that contains the epsilon constraint
-		// and the original and constraint
+		// the concatenation of an epsilon predicate and an and predicate is a new and predicate that contains the epsilon predicate
+		// and the original and predicate
 		if(a instanceof AndPredicate){
 			return new AndPredicate<S>(new EpsilonPredicate<S>(), ((AndPredicate<S>) a).getConstraints());
 		}
-		// the concatenation of an epsilon constraint and an or constraint is a new and constraint that contains the epsilon constraint
-		// and the original or constraint
+		// the concatenation of an epsilon predicate and an or predicate is a new and predicate that contains the epsilon predicate
+		// and the original or predicate
 		if(a instanceof OrPredicate){
 			return new AndPredicate<S>(this, a);
 		}
-
-		throw new IllegalArgumentException("The type:"+a.getClass()+" of the constraint is not in the set of the predefined types");
+		// is generated if the type of the predicate is not supported by the predicate computation
+		throw new IllegalArgumentException("The type:"+a.getClass()+" of the predicate is not in the set of the predefined types");
 	}
 
 	
+	
+
+	/** the union operator applied to an epsilon predicate is defined as follows:
+	 * -	the union of an epsilon predicate and a lambda predicate is the lambda predicate
+	 * -	the union of an epsilon predicate and an EmptyPredicate is the epsilon predicate
+	 * -	the union of an epsilon predicate and an epsilon predicate is the epsilon predicate
+	 * -	the union of an epsilon predicate and an epsilon predicate is an or predicate that contains the epsilon predicate and the predicate
+	 * -	the union of an epsilon predicate and an and predicate is an or predicate that contains the epsilon predicate and the and predicate 
+	 * @param a: is the predicate to be unified
+	 * @return the predicate which is the union of the epsilon predicate and a
+	 * @throws IllegalArgumentException is generated when the predicate to be concatenated is null
+	 */
+	@Override
+	public AbstractPredicate<S> union(AbstractPredicate<S> a) {
+		if(a==null){
+			throw new IllegalArgumentException("The predicate to be concatenated cannot be null");
+		}
+		// the union of an epsilon predicate and a lambda predicate is the lambda predicate
+		if(a instanceof LambdaPredicate){
+			return new LambdaPredicate<S>();
+		}
+		//  the union of an epsilon predicate and an EmptyPredicate is the epsilon predicate
+		if(a instanceof EmptyPredicate){
+			return this;
+		}
+		//  the union of an epsilon predicate and an epsilon predicate is the epsilon predicate
+		if(a instanceof EpsilonPredicate){
+			return this;
+		}
+		//  the union of an epsilon predicate and an epsilon predicate is an or predicate that contains the epsilon predicate and the predicate
+		if(a instanceof Predicate){
+			return new OrPredicate<S>(this, a);
+		}
+		//  the union of an epsilon predicate and an or predicate is an or predicate that contains the epsilon predicate and the or predicate 
+		if(a instanceof OrPredicate){
+			return new OrPredicate<S>(this, a);
+		}
+		//  the union of an epsilon predicate and an and predicate is an or predicate that contains the epsilon predicate and the and predicate 
+		if(a instanceof AndPredicate){
+			return new OrPredicate<S>(this, ((AndPredicate<S>) a).getConstraints());
+		}
+		throw new IllegalArgumentException("The type:"+a.getClass()+" of the predicate is not in the set of the predefined types");
+	}
 	/**
-	 * the star operator applied to an epsilon constraint returns the epsilon constraint
-	 * @return the epsilon constraint
+	 * the star operator applied to an epsilon predicate returns the epsilon predicate
+	 * @return the epsilon predicate
 	 */
 	@Override
 	public AbstractPredicate<S> star() {
 		return new EpsilonPredicate<S>();
 	}
-
-	/** the union operator applied to an epsilon constraint is defined as follows:
-	 * -	the union of an epsilon constraint and a lambda constraint is the lambda constraint
-	 * -	the union of an epsilon constraint and an EmptyConstraint is the epsilon constraint
-	 * -	the union of an epsilon constraint and an epsilon constraint is the epsilon constraint
-	 * -	the union of an epsilon constraint and an epsilon constraint is an or constraint that contains the epsilon constraint and the predicate
-	 * -	the union of an epsilon constraint and an and constraint is an or constraint that contains the epsilon constraint and the and constraint 
-	 * @param a: is the constraint to be unified
-	 * @return the constraint which is the union of the epsilon constraint and a
-	 * @throws IllegalArgumentException is generated when the constraint to be concatenated is null
+	/**
+	 * the omega operator applied to an epsilon predicate returns the epsilon predicate
+	 * @return the epsilon predicate
 	 */
 	@Override
-	public AbstractPredicate<S> union(AbstractPredicate<S> a) {
-		if(a==null){
-			throw new IllegalArgumentException("The constraint to be concatenated cannot be null");
-		}
-		// the union of an epsilon constraint and a lambda constraint is the lambda constraint
-		if(a instanceof LambdaPredicate){
-			return new LambdaPredicate<S>();
-		}
-		//  the union of an epsilon constraint and an EmptyConstraint is the epsilon constraint
-		if(a instanceof EmptyPredicate){
-			return this;
-		}
-		//  the union of an epsilon constraint and an epsilon constraint is the epsilon constraint
-		if(a instanceof EpsilonPredicate){
-			return this;
-		}
-		//  the union of an epsilon constraint and an epsilon constraint is an or constraint that contains the epsilon constraint and the predicate
-		if(a instanceof Predicate){
-			return new OrPredicate<S>(this, a);
-		}
-		//  the union of an epsilon constraint and an or constraint is an or constraint that contains the epsilon constraint and the or constraint 
-		if(a instanceof OrPredicate){
-			return new OrPredicate<S>(this, a);
-		}
-		//  the union of an epsilon constraint and an and constraint is an or constraint that contains the epsilon constraint and the and constraint 
-		if(a instanceof AndPredicate){
-			return new OrPredicate<S>(this, ((AndPredicate<S>) a).getConstraints());
-		}
-		throw new IllegalArgumentException("The type:"+a.getClass()+" of the constraint is not in the set of the predefined types");
+	public AbstractPredicate<S> omega() {
+		return this;
+	}
+	/**
+	 * simplify the predicate. The simplification of the epsilon predicate is the epsilon predicate its-self
+	 * @return the epsilon predicate
+	 */
+	@Override
+	public AbstractPredicate<S> simplify() {
+		return this;
 	}
 
 	/**
@@ -129,17 +147,4 @@ public class EpsilonPredicate<S extends State> extends AbstractPredicate<S>{
 	public int hashCode() {
 		return 132;
 	}
-
-
-	@Override
-	public AbstractPredicate<S> omega() {
-		return this;
-	}
-
-
-	@Override
-	public AbstractPredicate<S> simplify() {
-		return this;
-	}
-
 }
