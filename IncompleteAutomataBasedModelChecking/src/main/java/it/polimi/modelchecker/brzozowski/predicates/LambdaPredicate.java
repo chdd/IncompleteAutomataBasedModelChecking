4256ac9e153a172e9383dbf5a3e7f22c3d3b2997
claudiomenghi <cla.menghi@gmail.com>
2014-09-14 11:54:53 +0200

    LambdaPredicate, Javadoc updated

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/modelchecker/brzozowski/predicates/LambdaPredicate.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/modelchecker/brzozowski/predicates/LambdaPredicate.java
@@ -4,15 +4,15 @@ import it.polimi.model.State;
 
 /**
  * @author claudiomenghi
- * contains a lambda predicate. The lambda predicate is used to identify states that are accepting in the BA (final in the FSA)
+ * contains a {@link LambdaPredicate}. The {@link LambdaPredicate} is used to identify states that are accepting in the BA (final in the FSA)
  */
 public class LambdaPredicate<S extends State> extends AbstractPredicate<S> {
 
 	/**
-	 * the concatenation of a lambda predicate with another predicate is equal to the other predicate
-	 * @param a: is the predicate to be concatenated
-	 * @return the predicate a since the lambda operator has no effect
-	 * @throws IllegalArgumentException is generated when the predicate to be concatenated is null
+	 * the concatenation of a {@link LambdaPredicate} predicate with another {@link AbstractPredicate} is equal to the other {@link AbstractPredicate}
+	 * @param a: is the {@link AbstractPredicate} to be concatenated
+	 * @return the {@link AbstractPredicate} a since the {@link LambdaPredicate} has no effect
+	 * @throws IllegalArgumentException is generated when the {@link AbstractPredicate} to be concatenated is null
 	 */
 	@Override
 	public AbstractPredicate<S> concatenate(AbstractPredicate<S> a) {
@@ -23,11 +23,11 @@ public class LambdaPredicate<S extends State> extends AbstractPredicate<S> {
 	}
 
 	/**
-	 * the union of a lambda predicate and the predicate a is equal to the lambda predicate if a is an {@link EmptyPredicate} or 
-	 * to a {@link LambdaPredicate} while is equal to a new {@link OrPredicate} that contains the {@link LambdaPredicate} 
-	 * and the predicate a in the other cases
+	 * the union of a {@link LambdaPredicate} and the {@link AbstractPredicate} a 
+	 * is equal to the {@link LambdaPredicate} if a is an {@link EmptyPredicate} or a {@link LambdaPredicate}
+	 * while is equal to a new {@link OrPredicate} that contains the {@link LambdaPredicate} and the {@link AbstractPredicate} a in the other cases
 	 * @param the {@link AbstractPredicate} a to be concatenated with the {@link LambdaPredicate}
-	 * @throws IllegalArgumentException is generated if the constraint to be concatenated is null
+	 * @throws IllegalArgumentException is generated if the {@link AbstractPredicate} to be concatenated is null
 	 */
 	@Override
 	public AbstractPredicate<S> union(AbstractPredicate<S> a) {
@@ -65,23 +65,15 @@ public class LambdaPredicate<S extends State> extends AbstractPredicate<S> {
 	}
 	
 	/**
-	 * The simplify operator applied to a {@link LambdaPredicate} returns the {@link LambdaPredicate}
-	 * @return the {@link LambdaPredicate}
-	 */
-	@Override
-	public AbstractPredicate<S> simplify() {
-		return this;
-	}
-	
-	/**
-	 * see {@link AbstractPredicate}
+	 * @see {@link AbstractPredicate}
 	 */
 	@Override
 	public String toString() {
 		return "λ";
 	}
+	
 	/**
-	 * see {@link Object}
+	 * @see {@link Object}
 	 */
 	@Override
 	public boolean equals(Object o){
@@ -90,7 +82,10 @@ public class LambdaPredicate<S extends State> extends AbstractPredicate<S> {
 		}
 		return false;
 	}
-
+	
+	/**
+	 * @see {@link Object}
+	 */
 	@Override
 	public int hashCode() {
 		return 555;
