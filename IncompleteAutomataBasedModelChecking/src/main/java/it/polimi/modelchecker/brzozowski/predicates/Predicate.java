f1f81746e71ddcf7ae5d0eead3046a08f2f7d916
claudiomenghi <cla.menghi@gmail.com>
2014-09-14 11:55:48 +0200

    Predicate, Javadoc updated

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/modelchecker/brzozowski/predicates/Predicate.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/modelchecker/brzozowski/predicates/Predicate.java
@@ -3,18 +3,20 @@ package it.polimi.modelchecker.brzozowski.predicates;
 import it.polimi.model.State;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author claudiomenghi
- * contains a predicate constraint
+ * contains a {@link Predicate} which constraints a state to be able to recognize a particular regular expression
  */
 public class Predicate<S extends State> extends AbstractPredicate<S>{
 
 	/**
 	 * contains the state of the predicate
 	 */
-	private S state;
+	private final S state;
 	/**
 	 * contains the regular expression of the predicate
 	 */
@@ -24,6 +26,7 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 	 * creates a new predicate
 	 * @param state is the state that is constrained
 	 * @param regularExpression is the regular expression that the state must satisfy
+	 * @throws IllegalArgumentException if the state or the regular expression of the predicate are null
 	 */
 	public Predicate(S state, String regularExpression){
 		if(state==null){
@@ -42,17 +45,7 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 	public S getState() {
 		return state;
 	}
-	/**
-	 * set the state of the predicate
-	 * @param state: the state to be set
-	 * @throws IllegalArgumentException if the state to be set is null
-	 */
-	public void setState(S state) {
-		if(state==null){
-			throw new IllegalArgumentException("The state to be set cannot be null");
-		}
-		this.state = state;
-	}
+	
 	/**
 	 * returns the regular expression of the predicate
 	 * @return the regular expression that constraints the predicate
@@ -74,20 +67,20 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 	
 	
 	/**
-	 * a predicate this concatenated with a constraint a is obtained as follows:
-	 * -	the concatenation of the predicate and the empty constraint is empty
-	 * -	the concatenation of the predicate and lambda is equal to the predicate
-	 * -	the concatenation of two predicate with the same state constrained is a new predicate where the regular expressions are concatenated
-	 *		the concatenation of two predicate with different state constrained is a new and constraint that contains the two predicates
-	 * -	the concatenation of a predicate and an AndConstraint mixes the regular expression of this predicate of the regular
-	 *  	expression of the first predicate of the and constraint if they have the same state
-	 * -	the concatenation of a predicate and an AndConstraint add the predicate to the and constraint if this predicate and
-	 *  	the first predicate of the and constraint have a different state
-	 * -	the concatenation of a predicate and an OrConstraint creates a new AndConstraint where the first element is the predicate 
-	 * 		and the second one is the or constraint
-	 * -	the concatenation of a predicate and an EpsilonConstraint creates a new AndConstraint where the first element is the predicate 
-	 * 		and the second one is the EpsilonConstraint
-	 * @param a the constraint to be concatenated
+	 * a predicate this concatenated with an {@link AbstractPredicate} a is obtained as follows:
+	 * -	the concatenation of the {@link Predicate} and the {@link EmptyPredicate} is an {@link EmptyPredicate}
+	 * -	the concatenation of the {@link Predicate} and {@link LambdaPredicate} is equal to the {@link Predicate}
+	 * -	the concatenation of two {@link Predicate} with the same state is a new {@link Predicate} where the regular expressions are concatenated
+	 *		the concatenation of two {@link Predicate} with different states  is a new and {@link AndPredicate} that contains the two predicates
+	 * -	the concatenation of a {@link Predicate} and an {@link AndPredicate} mixes the regular expression of this predicate of the regular
+	 *  	expression of the first predicate of the and predicate if they refer to the same state
+	 * -	the concatenation of a {@link Predicate} and an {@link AndPredicate} add the predicate to the {@link AndPredicate}  if this predicate and
+	 *  	the first predicate of the {@link AndPredicate} have a different states
+	 * -	the concatenation of a {@link Predicate} and an {@link OrPredicate} creates a new {@link AndPredicate} where the first element is the {@link Predicate} 
+	 * 		and the second one is the {@link OrPredicate}
+	 * -	the concatenation of a {@link Predicate} and an {@link EpsilonPredicate} creates a new {@link AndPredicate} where the first element is the {@link Predicate} 
+	 * 		and the second one is the {@link EpsilonPredicate}
+	 * @param a the {@link AbstractPredicate} to be concatenated
 	 * @throws IllegalArgumentException if a is null or if the constraint type is not supported
 	 */
 	@Override
@@ -118,7 +111,6 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 				AndPredicate<S> cret=new AndPredicate<S>(l);
 				return cret;
 			}
-			
 		}
 		/* -	the concatenation of a predicate and an AndConstraint mixes the regular expression of this predicate of the regular
 		 *  	expression of the first predicate of the and constraint if they have the same state
@@ -126,12 +118,13 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 		 *  	the first predicate of the and constraint have a different state
 		 */
 		if(a instanceof AndPredicate){
-			AndPredicate<S> atmp=(AndPredicate<S>)a;
-			if(atmp.getFistPredicate() instanceof Predicate &&
-					this.state.equals(((Predicate<S>)atmp.getFistPredicate()).state)){
+			AndPredicate<S> andPredicate=(AndPredicate<S>)a;
+			
+			if(andPredicate.getFistPredicate() instanceof Predicate &&
+					this.state.equals(((Predicate<S>)andPredicate.getFistPredicate()).state)){
 						List<AbstractPredicate<S>> l=new ArrayList<AbstractPredicate<S>>();
-						l.add(new Predicate<S>(this.state, this.regularExpression.concat(((Predicate<S>)atmp.getFistPredicate()).regularExpression)));
-						l.addAll(atmp.getPredicates().subList(1, atmp.getPredicates().size()));
+						l.add(new Predicate<S>(this.state, this.regularExpression.concat(((Predicate<S>)andPredicate.getFistPredicate()).regularExpression)));
+						l.addAll(andPredicate.getPredicates().subList(1, andPredicate.getPredicates().size()));
 						AndPredicate<S> cret=new AndPredicate<S>(l);;
 						return cret;
 				}
@@ -139,7 +132,7 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 			else{
 				List<AbstractPredicate<S>> l=new ArrayList<AbstractPredicate<S>>();
 				l.add(this);
-				l.addAll(atmp.getPredicates());
+				l.addAll(andPredicate.getPredicates());
 				
 				AndPredicate<S> cret=new AndPredicate<S>(l);
 				return cret;
@@ -150,10 +143,8 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 		*/
 		if(a instanceof OrPredicate){
 			List<AbstractPredicate<S>> l=new ArrayList<AbstractPredicate<S>>();
-			
-			for(AbstractPredicate<S> s: ((OrPredicate<S>) a).getPredicates()){
-				l.add(this.concatenate(s));
-			}
+			l.add(this);
+			l.add(a);
 			
 			AndPredicate<S> cret=new AndPredicate<S>(l);
 			return cret;
@@ -175,27 +166,18 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 	}
 	
 	/**
-	 * the star operator applied to a predicate <s,exp> modifies the regular expression into (exp)* generating the new constraint <s,(exp)*>
-	 * @return the new constraint where the star operator is applied to the predicate
-	 */
-	@Override
-	public AbstractPredicate<S> star() {
-		return new Predicate<S>(this.state, "("+this.regularExpression+")*");
-	}
-	
-	/**
-	 * the union of a predicate and a constraint a is defined as follows:
-	 * -	the union of a predicate and the empty constraint is equal to the predicate
-	 * - 	the union of a predicate and lambda is a new or constraint that contains the predicate and lambda
-	 * -	the union of two predicates <s, reg1>, <s, reg2> with the same state s is a new predicate that contains the or combination 
-	 * 		of their regular expressions <s, (reg1)+(reg2)> 
-	 * -	the union of two predicates <s1, reg1>, <s2, reg2> with a different state is a new or constraint that contains the two predicates 
-	 * -	the union of the predicate and an or constraint is a new or constraint where the predicate is added
-	 * -	the union of the predicate and an and constraint is a new or constraint where the predicate and the and constraint are added
-	 * -	the union of a predicate and epsilon is a new or constraint that contains the predicate
-	 * @param a the constraint to be unified with this object
-	 * @return the union of this predicate and the constraint a
-	 * @throws IllegalArgumentException id the constraint a is null or the type of constraint is not supported
+	 * the union of a {@link Predicate} and a {@link AbstractPredicate} a is defined as follows:
+	 * -	the union of a {@link Predicate} and the {@link EmptyPredicate}  is equal to the {@link Predicate}
+	 * - 	the union of a {@link Predicate} and {@link LambdaPredicate} is a new or {@link OrPredicate} that contains the {@link Predicate} and {@link LambdaPredicate}
+	 * -	the union of two {@link Predicate} <state, expression1>, <state, expression2> with the same state s is a new {@link Predicate} that contains the or combination 
+	 * 		of their regular expressions <state, (expression1)+(expression2)> 
+	 * -	the union of two {@link Predicate} <state1, expression1>, <state2, expression2> with a different state is a new {@link OrPredicate}  that contains the two {@link Predicate} 
+	 * -	the union of the {@link Predicate} and an {@link OrPredicate} is a new {@link OrPredicate} where the {@link Predicate} is added
+	 * -	the union of the {@link Predicate} and an {@link AndPredicate} is a new {@link OrPredicate} where the {@link Predicate} and the {@link OrPredicate} are added
+	 * -	the union of a {@link Predicate} and {@link EpsilonPredicate} is a new {@link OrPredicate} that contains the {@link Predicate} and the {@link EpsilonPredicate}
+	 * @param a the {@link AbstractPredicate} to be unified with this {@link Predicate}
+	 * @return the union of this {@link Predicate} and the {@link AbstractPredicate} a
+	 * @throws IllegalArgumentException id the {@link AbstractPredicate} a is null or the type of predicate is not supported
 	 */
 	@Override
 	public AbstractPredicate<S> union(AbstractPredicate<S> a) {
@@ -208,14 +190,20 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 		}
 		// the union of a predicate and lambda is a new or constraint that contains the predicate and lambda
 		if(a instanceof LambdaPredicate){
-			return this;
+			Set<AbstractPredicate<S>> predicates=new HashSet<AbstractPredicate<S>>();
+			predicates.add(this);
+			predicates.add(a);
+			return new OrPredicate<>(predicates);
 		}
 		// if a is a predicate
 		if(a instanceof Predicate){
 			Predicate<S> a1=(Predicate<S>) a;
 			
+			if(this.equals(a1)){
+				return this;
+			}
 			// the union of two predicates <s, reg1>, <s, reg2> with the same state s is a new predicate that contains the or combination 
-			// of their regular expressions <s, (reg1)+(reg2)> 
+			// of their regular expressions <s, (reg1)+(reg2)>
 			if(a1.getState().equals(this.getState())){
 				return new Predicate<S>(this.getState(), "(("+this.regularExpression+")+("+a1.regularExpression+"))");
 			}
@@ -234,19 +222,37 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 		}
 		// the union of a predicate and epsilon is a new or constraint that contains the predicate
 		if(a instanceof EpsilonPredicate){
-			return this;
+			return new OrPredicate<>(this,a);
 		}
-		throw new IllegalArgumentException("The type:"+a.getClass()+" of the constraint is not in the set of the predefined types");
+		throw new IllegalArgumentException("The type:"+a.getClass()+" of the predicate is not in the set of the supported types");
 	}
 	
 	/**
-	 * see {@link AbstractPredicate}
+	 * the star operator applied to a {@link Predicate} <state, expression> modifies the regular expression into (expression)* generating the new {@link Predicate} <state,(expression)*>
+	 * @return the new {@link Predicate} where the star operator is applied to the regular expression
+	 */
+	@Override
+	public AbstractPredicate<S> star() {
+		return new Predicate<S>(this.state, "("+this.regularExpression+")*");
+	}
+	
+	/**
+	 * the omega operator applied to a {@link Predicate} <state, expression> modifies the regular expression into (expression)ω generating the new {@link Predicate} <state,(expression)ω>
+	 * @return the new {@link Predicate} where the omega operator is applied to the regular expression
+	 */
+	@Override
+	public AbstractPredicate<S> omega() {
+		return new Predicate<S>(this.getState(), "("+this.getRegularExpression()+")ω");
+	}
+	
+	/**
+	 * @see {@link AbstractPredicate}
 	 */
 	public String toString(){
 		return "<"+state+","+this.regularExpression+">";
 	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
+	/** 
+	 * @see {@link Object}
 	 */
 	@Override
 	public int hashCode() {
@@ -259,8 +265,8 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 		result = prime * result + ((state == null) ? 0 : state.hashCode());
 		return result;
 	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
+	/** 
+	 * @see {@link Object}
 	 */
 	@Override
 	public boolean equals(Object obj) {
@@ -284,13 +290,4 @@ public class Predicate<S extends State> extends AbstractPredicate<S>{
 			return false;
 		return true;
 	}
-	@Override
-	public AbstractPredicate<S> omega() {
-		return new Predicate<S>(this.getState(), "("+this.getRegularExpression()+")ω");
-	}
-	@Override
-	public AbstractPredicate<S> simplify() {
-		return this;
-	}
-	
 }
