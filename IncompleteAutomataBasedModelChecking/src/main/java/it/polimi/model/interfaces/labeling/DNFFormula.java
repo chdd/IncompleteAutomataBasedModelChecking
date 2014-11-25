22a0ae9360507411bebdc18e66b3aa18d90a678b
claudiomenghi <cla.menghi@gmail.com>
2014-11-25 08:41:39 +0100

    Labeling of the Transition modified

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/model/interfaces/labeling/DNFFormula.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/model/interfaces/labeling/DNFFormula.java
@@ -1,6 +1,11 @@
 package it.polimi.model.interfaces.labeling;
 
+import java.util.Set;
+
 public interface DNFFormula extends Formula {
 
 	public void addDisjunctionClause(Formula clause);
+	public Set<ConjunctiveClause> getConjunctiveClauses();
+	
+	public void addDisjunctionClauses(Set<ConjunctiveClause> clauses);
 }
