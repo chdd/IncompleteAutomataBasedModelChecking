17393088129beea3a684389d2692f88dccfd3b1a
claudiomenghi <cla.menghi@gmail.com>
2014-11-26 08:38:16 +0100

    Main updated with the new use of generics

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/Main.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/Main.java
@@ -1,73 +1,42 @@
 package it.polimi;
 
-import java.io.File;
-
 import it.polimi.controller.Controller;
 import it.polimi.model.Model;
 import it.polimi.model.ModelInterface;
 import it.polimi.model.impl.states.IntersectionState;
-import it.polimi.model.impl.states.IntersectionStateFactory;
+import it.polimi.model.impl.states.IntersectionStateFactoryImpl;
 import it.polimi.model.impl.states.State;
-import it.polimi.model.impl.states.StateFactory;
+import it.polimi.model.impl.states.StateFactoryImpl;
 import it.polimi.model.impl.transitions.ConstrainedTransitionFactoryImpl;
-import it.polimi.model.impl.transitions.LabelledTransition;
-import it.polimi.model.impl.transitions.LabelledTransitionFactoryImpl;
-import it.polimi.model.interfaces.transitions.ConstrainedTransitionFactory;
-import it.polimi.model.interfaces.transitions.LabelledTransitionFactory;
+import it.polimi.model.impl.transitions.Transition;
+import it.polimi.model.impl.transitions.TransitionFactoryImpl;
 import it.polimi.view.View;
 import it.polimi.view.ViewInterface;
 
-
-public class Main{
+public class Main {
 
 	public static void main(String args[]) {
-		
-		System.out.println(new File(Constants.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
-		System.out.println(new File(Constants.class.getProtectionDomain().getCodeSource().getLocation().getPath()));
-		// creates the model of the application starting from the model of the system contained in modelPath and the specification contained in the specificationPath
-		ModelInterface<State,
-			State, StateFactory<State>, 
-			LabelledTransition<State>, 
-			LabelledTransitionFactory<State, LabelledTransition<State>>, 
-			IntersectionState<State>, 
-			IntersectionStateFactory<State,IntersectionState<State>>, 
-			LabelledTransition<State>, 
-			ConstrainedTransitionFactory<State,LabelledTransition<State>>> model=new Model<State,
-					State, StateFactory<State>, 
-					LabelledTransition<State>, 
-					LabelledTransitionFactory<State, LabelledTransition<State>>, 
-					IntersectionState<State>, 
-					IntersectionStateFactory<State,IntersectionState<State>>, 
-					LabelledTransition<State>, 
-					ConstrainedTransitionFactory<State,LabelledTransition<State>>>(
-							new StateFactory<State>(),
-							new LabelledTransitionFactoryImpl(),
-							new IntersectionStateFactory<State, IntersectionState<State>>(),
-							new ConstrainedTransitionFactoryImpl());
-		
+
+		// creates the model of the application starting from the model of the
+		// system contained in modelPath and the specification contained in the
+		// specificationPath
+		ModelInterface<State, State, Transition, IntersectionState<State>, Transition> model = 
+				new Model<State, State, Transition, IntersectionState<State>, Transition>(
+				new StateFactoryImpl(), new TransitionFactoryImpl(),
+				new IntersectionStateFactoryImpl(),
+				new ConstrainedTransitionFactoryImpl());
+
 		// contains the view of the application
-		ViewInterface<State, State, LabelledTransition<State>,IntersectionState<State>, LabelledTransition<State>,
-			LabelledTransitionFactory<State, LabelledTransition <State>>,
-			ConstrainedTransitionFactory<State, LabelledTransition<State>>> view=
-			new View<State, State, StateFactory<State>, LabelledTransition<State>, LabelledTransitionFactory<State, LabelledTransition<State>>, IntersectionState<State>, 
-			IntersectionStateFactory<State, IntersectionState<State>>,
-			LabelledTransition<State>,
-			ConstrainedTransitionFactory<State, LabelledTransition<State>>>(
-				 model);
-		
+		ViewInterface<State, State, Transition, IntersectionState<State>, Transition> view =
+				new View<State, State, Transition, IntersectionState<State>, Transition>(
+				model);
+
 		// creates a new controller with the specified model and view
-		Controller<State, State, StateFactory<State>, LabelledTransition<State>, LabelledTransitionFactory<State, LabelledTransition<State>>, 
-					IntersectionState<State>, IntersectionStateFactory<State,IntersectionState<State>>,
-					LabelledTransition<State>, ConstrainedTransitionFactory<State,LabelledTransition<State>>> controller=
-					new Controller<State, State, StateFactory<State>, LabelledTransition<State>, LabelledTransitionFactory<State, LabelledTransition<State>>, 
-							IntersectionState<State>, IntersectionStateFactory<State,IntersectionState<State>>,
-							LabelledTransition<State>, ConstrainedTransitionFactory<State,LabelledTransition<State>>>(model, view);
-		
-		// add  the controller as observer of the view
+		Controller<State, State, Transition, IntersectionState<State>, Transition> controller = new Controller<State, State, Transition, IntersectionState<State>,  Transition>(
+				model, view);
+
+		// add the controller as observer of the view
 		view.addObserver(controller);
-		
-		
-		
-		
+
 	}
 }
