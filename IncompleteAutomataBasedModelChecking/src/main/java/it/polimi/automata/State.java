072d634cdfad4a11c9a679693ca28cb5bd8bc5d1
claudiomenghi <cla.menghi@gmail.com>
2015-01-13 20:28:12 +0100

    Documentation to the interface State added

---
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/State.java
@@ -0,0 +1,29 @@
+package it.polimi.automata;
+
+import it.polimi.model.impl.states.StateImpl;
+
+/**
+ * <p>Represents a state of an automaton. <br>
+ * A state is identified by an id and has a name</p>
+ * 
+ * @author claudiomenghi
+ * @see StateImpl
+ */
+public interface State {
+	
+	/**
+	 * <p>returns the <br>id</br> of the state<br>
+	 * The id uniquely identifies the state
+	 * </p>
+	 * 
+	 * @return the <br>id</br> of the state
+	 */
+	public int getId();
+
+	/**
+	 * returns the <br>name</br> of the state
+	 * 
+	 * @return the <br>name</br> of the state
+	 */
+	public String getName();
+}
