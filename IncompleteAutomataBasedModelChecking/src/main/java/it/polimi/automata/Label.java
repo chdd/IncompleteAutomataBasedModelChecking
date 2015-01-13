5332a7a1e5ea5284fab3c47f5d958a7e28ac13b8
claudiomenghi <cla.menghi@gmail.com>
2015-01-13 20:57:34 +0100

    Interface to the label of the transition added

---
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/Label.java
@@ -0,0 +1,18 @@
+package it.polimi.automata;
+
+
+/**
+ * <p>
+ * Represents the label of a transition. It can be a set of characters of the 
+ * alphabets or a propositional logic formula depending on whether the label
+ * is associated with a transition of the model or the claim, respectively.
+ * @see Transition
+ * @see State
+ * @see BA
+ * </p>
+ * 
+ * @author claudiomenghi
+ */
+public interface Label {
+
+}
