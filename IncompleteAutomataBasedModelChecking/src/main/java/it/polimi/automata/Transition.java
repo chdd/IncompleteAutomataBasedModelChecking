f417aabf529a3a161799d5d296609bf57c4955f4
claudiomenghi <cla.menghi@gmail.com>
2015-01-13 20:53:31 +0100

    Interface to a transition of the Buchi Automaton added

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/Transition.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/Transition.java
@@ -3,19 +3,39 @@ package it.polimi.automata;
 import it.polimi.model.impl.transitions.TransitionImpl;
 
 /**
- * <p>Represents a transition of an automaton. <br>
- * A transition is identified by an id and is labeled by characters</p>
+ * <p>
+ * Represents a transition of an automaton. <br>
+ * A transition is identified by an id and is labeled by characters
+ * </p>
  * 
  * @author claudiomenghi
  * @see TransitionImpl
+ * @param <LABEL>
+ *            is the label of the transition which must extend the interface 
+ *            {@link Label}. Depending on whether the transition is associated 
+ *            with the model or a property it is associated with a set of atomic
+ *            propositions or a propositional logic formula, respectively.
  */
-public interface Transition {
+public interface Transition<LABEL extends Label> {
 
 	/**
-	 * returns the id of the transition
+	 * <p>
+	 * returns the <b>id</b> of the transition
+	 * </p>
 	 * 
-	 * @return the id of the transition
+	 * @return the <b>id</b> of the transition
 	 */
 	public int getId();
-	
+
+	/**
+	 * <p>
+	 * returns <b>the label</b> associated with the transition. Depending on
+	 * whether the transition is associated with the model or a property it is
+	 * associated with a set of atomic propositions or a propositional logic
+	 * formula, respectively.
+	 * </p>
+	 * 
+	 * @return the <b>label</b> associated with the transition
+	 */
+	public LABEL getLabel();
 }
