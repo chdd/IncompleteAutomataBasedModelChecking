a21b9c0c61ad8036c630698b8d87bec1c5821223
claudiomenghi <cla.menghi@gmail.com>
2015-01-14 14:10:00 +0100

    Factories of the BA and IBA updated as extension of the Factory class

--- IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/factories/IBAFactory.java
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/factories/IBAFactory.java
@@ -1,23 +1,21 @@
 package it.polimi.automata.factories;
 
-import it.polimi.automata.BA;
+import it.polimi.automata.IBA;
 import it.polimi.automata.State;
 import it.polimi.automata.Transition;
 import it.polimi.automata.labeling.Label;
 
-import org.apache.commons.collections15.Factory;
-
 /**
  * <p>
- * Represents the factory used to create a Buchi Automaton <br>
+ * Represents the factory used to create an Incomplete Buchi Automaton <br>
  * </p>
  * 
  * @author claudiomenghi
  * @param <STATE>
- *            is the type of the state of the Buchi Automaton. The type of the
- *            states of the automaton must implement the interface {@link State}
+ *            is the type of the state of the Incomplete Buchi Automaton. The type of the
+ *            states of the Incomplete automaton must implement the interface {@link State}
  * @param <TRANSITION>
- *            is the type of the transition of the Buchi Automaton. The typer of
+ *            is the type of the transition of the Incomplete Buchi Automaton. The type of
  *            the transitions of the automaton must implement the interface
  *            {@link Transition}
  * @param <LABEL>
@@ -25,11 +23,10 @@ import org.apache.commons.collections15.Factory;
  *            the automaton represents the model or the claim it is a set of
  *            proposition or a propositional logic formula {@link Label}
  */
-public interface BAFactory<LABEL extends Label, STATE extends State, TRANSITION extends Transition<LABEL>>
-		extends Factory<BA<LABEL, STATE, TRANSITION>> {
+public interface IBAFactory<LABEL extends Label, STATE extends State, TRANSITION extends Transition<LABEL>>{
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public BA<LABEL, STATE, TRANSITION> create();
+	public IBA<LABEL, STATE, TRANSITION> create();
 }
