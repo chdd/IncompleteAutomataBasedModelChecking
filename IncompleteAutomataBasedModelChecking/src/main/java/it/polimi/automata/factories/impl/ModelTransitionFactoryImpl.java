f0e0472ff4f9c1e8e6a1d55463866a83d84a1889
claudiomenghi <cla.menghi@gmail.com>
2015-01-15 12:23:23 +0100

    Factories of the transitions of the model and the claim modified

---
+++ IncompleteAutomataBasedModelChecking/src/main/java/it/polimi/automata/factories/impl/ModelTransitionFactoryImpl.java
@@ -0,0 +1,55 @@
+package it.polimi.automata.factories.impl;
+
+import it.polimi.automata.Transition;
+import it.polimi.automata.labeling.Label;
+
+import java.util.Set;
+
+import rwth.i2.ltl2ba4j.model.IGraphProposition;
+
+public class ModelTransitionFactoryImpl<LABEL extends Label> extends
+		ClaimTransitionFactoryImpl<LABEL> {
+	/**
+	 * {@inheritDoc} <br>
+	 * also guarantees that no transition with a negated proposition is created
+	 * @throws IllegalArgumentException if one of the proposition in the label is negated
+	 */
+	public Transition<LABEL> create(Set<LABEL> labels) {
+		if (labels == null) {
+			throw new NullPointerException(
+					"The labels to be added at the Transition cannot be null");
+		}
+		for(LABEL label: labels){
+			for(IGraphProposition p:label.getAtomicPropositions()){
+				if(p.isNegated()){
+					throw new IllegalArgumentException("The propositions of the model cannot be negated");
+				}
+			}
+		}
+		Transition<LABEL> t=super.create(labels);
+
+		return t;
+	}
+
+	/**
+	 * {@inheritDoc} <br>
+	 * also guarantees that no transition with a negated proposition is created
+	 * @throws IllegalArgumentException if one of the proposition in the label is negated
+	 */
+	public Transition<LABEL> create(int id, Set<LABEL> labels) {
+		if (labels == null) {
+			throw new NullPointerException(
+					"The labels to be added at the Transition cannot be null");
+		}
+		for(LABEL label: labels){
+			for(IGraphProposition p:label.getAtomicPropositions()){
+				if(p.isNegated()){
+					throw new IllegalArgumentException("The propositions of the model cannot be negated");
+				}
+			}
+		}
+		Transition<LABEL> t=super.create(id, labels);
+
+		return t;
+	}
+}
