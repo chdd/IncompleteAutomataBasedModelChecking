acf9e6a5bd7cca5da81a7be99231b13c84ea554e
claudiomenghi <cla.menghi@gmail.com>
2015-01-20 17:56:15 +0100

    Test of the State class added

--- IncompleteAutomataBasedModelChecking/CHIAAutomata/src/test/java/it/polimi/automata/state/impl/StateImplTest.java
+++ IncompleteAutomataBasedModelChecking/CHIAAutomata/src/test/java/it/polimi/automata/state/impl/StateImplTest.java
@@ -1,91 +1,129 @@
+/**
+ * 
+ */
 package it.polimi.automata.state.impl;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests the class {@link StateImpl}
- * 
  * @author claudiomenghi
  * 
  */
 public class StateImplTest {
 
-	private static final String name1 = "name";
-	private static final String name2 = "name2";
-	private static final String emptyName = "";
-	private static final int id1 = 1;
-	private static final int id2 = 2;
+	private StateImpl state1;
+	private StateImpl state2;
+	private StateImpl state3;
+	private StateImpl state4;
+	private StateImpl state5;
+	private StateImpl state6;
 
-	/**
-	 * Tests the constructor of the class StateImpl
-	 */
-	@Test
-	public void constructorTest() {
-		StateImpl state = new StateImpl(StateImplTest.name1, StateImplTest.id1);
-		assertTrue(state.id == StateImplTest.id1);
-		assertTrue(state.name == StateImplTest.name1);
-
-		state = new StateImpl(StateImplTest.id1);
-		assertTrue(state.id == StateImplTest.id1);
-		assertTrue(state.name == emptyName);
-	}
-
-	/**
-	 * Tests the method getId of the class StateImpl
-	 */
-	@Test
-	public void getIdTest() {
-		StateImpl state = new StateImpl(StateImplTest.name1, StateImplTest.id1);
-		assertTrue(state.getId() == StateImplTest.id1);
-
-		state = new StateImpl(StateImplTest.id1);
-		assertTrue(state.getId() == StateImplTest.id1);
-	}
-
-	/**
-	 * Tests the behavior of the method getName
-	 */
-	public void getNameTest() {
-		StateImpl state = new StateImpl(StateImplTest.name1, StateImplTest.id1);
-		assertTrue(state.getName() == StateImplTest.name1);
-
-		state = new StateImpl(StateImplTest.id1);
-		assertTrue(state.getName() == emptyName);
-	}
-
-	/**
-	 * Tests the behavior of the method setName
-	 */
-	public void setNameTest() {
-		StateImpl state = new StateImpl(StateImplTest.name1, StateImplTest.id1);
-		assertTrue(state.getName() == StateImplTest.name1);
-
-		state.setName(StateImplTest.name2);
-		assertTrue(state.name == name2);
-		assertTrue(state.getName() == StateImplTest.name2);
-	}
-	
-	/**
-	 * Tests the behavior of the toString method
-	 */
-	public void toStringTest() {
-		StateImpl state = new StateImpl(StateImplTest.name1, StateImplTest.id1);
-		assertTrue(state.toString().equals(StateImplTest.id1+": "+StateImplTest.name1));
-	}
-	
-	/**
-	 * Tests the behavior of the toString method
-	 */
-	public void toStringHashCodeEquals() {
-		StateImpl state1 = new StateImpl(StateImplTest.name1, StateImplTest.id1);
-		StateImpl state2 = new StateImpl(StateImplTest.name2, StateImplTest.id1);
-		assertTrue(state1.equals(state2));
-		StateImpl state3 = new StateImpl(StateImplTest.name1, StateImplTest.id2);
-		assertFalse(state1.equals(state3));
-		assertFalse(state2.equals(state3));
-	}
+	@Before
+	public void setUp() {
+		this.state1 = new StateImpl(1);
+		this.state2 = new StateImpl(2);
+		this.state3 = new StateImpl(2);
 		
+		this.state4=new StateImpl("name1", 3);
+		this.state5=new StateImpl("name1", 4);
+		this.state6 = new StateImpl("name2", 4);
+		
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.state.impl.StateImpl#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		assertEquals(this.state2.hashCode(), this.state3.hashCode());
+		assertEquals(this.state5.hashCode(), this.state6.hashCode());
+		// note that the hashcode function only considers id of the states
+		assertFalse(this.state1.hashCode()==this.state2.hashCode());
+		assertFalse(this.state4.hashCode()==this.state5.hashCode());
+	}
+
+	/**
+	 * Test method for {@link it.polimi.automata.state.impl.StateImpl#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		assertEquals(this.state2.getId(), this.state3.getId());
+		assertEquals(this.state5.getId(), this.state6.getId());
+		// note that the hashcode function only considers id of the states
+		assertFalse(this.state1.getId()==this.state2.getId());
+		assertFalse(this.state4.getId()==this.state5.getId());
+
+	}
+
+	/**
+	 * Test method for {@link it.polimi.automata.state.impl.StateImpl#getName()}
+	 * .
+	 */
+	@Test
+	public void testGetName() {
+		assertEquals(this.state2.getName(), this.state3.getName());
+		assertEquals(this.state2.getName(), "");
+		assertEquals(this.state4.getName(), this.state5.getName());
+		assertEquals(this.state1.getName(), this.state2.getName());
+		assertFalse(this.state5.getName()==this.state6.getName());
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.state.impl.StateImpl#StateImpl(int)}.
+	 */
+	@Test
+	public void testStateImplInt() {
+		this.state4=new StateImpl(6);
+		assertEquals(6, this.state4.id);
+		assertEquals("", this.state4.name);
+		
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.state.impl.StateImpl#StateImpl(java.lang.String, int)}
+	 * .
+	 */
+	@Test
+	public void testStateImplStringInt() {
+		this.state4=new StateImpl("name", 6);
+		assertEquals(6, this.state4.id);
+		assertEquals("name", this.state4.name);
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.state.impl.StateImpl#setName(java.lang.String)}
+	 * .
+	 */
+	@Test
+	public void testSetName() {
+		this.state4.setName("prova");
+		assertEquals("prova", this.state4.name);
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.state.impl.StateImpl#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		assertEquals(this.state3.toString(), this.state3.getId()+": "+this.state3.getName());
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.state.impl.StateImpl#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		assertEquals(this.state2 , this.state3);
+		assertEquals(this.state5 , this.state6);
+	}
 
 }
