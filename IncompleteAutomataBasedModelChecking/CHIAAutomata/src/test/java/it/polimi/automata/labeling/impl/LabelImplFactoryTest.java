13920d86c065dee364a729dad670b40cb7f1c963
claudiomenghi <cla.menghi@gmail.com>
2015-01-21 16:13:46 +0100

    Test of the label factory added

---
+++ IncompleteAutomataBasedModelChecking/CHIAAutomata/src/test/java/it/polimi/automata/labeling/impl/LabelImplFactoryTest.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package it.polimi.automata.labeling.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import rwth.i2.ltl2ba4j.model.IGraphProposition;
+import rwth.i2.ltl2ba4j.model.impl.GraphProposition;
+
+/**
+ * @author claudiomenghi
+ * 
+ */
+public class LabelImplFactoryTest {
+
+	private LabelImplFactory factory;
+	private Set<IGraphProposition> labels;
+	private Set<IGraphProposition> labelsSet2;
+
+	
+	@Before
+	public void setUp() {
+		this.factory = new LabelImplFactory();
+		this.labels=new HashSet<IGraphProposition>();
+		this.labels.add(new GraphProposition("p", false));
+		this.labelsSet2=new HashSet<IGraphProposition>();
+		this.labelsSet2.add(new GraphProposition("p", false));
+		this.labelsSet2.add(new GraphProposition("c", false));
+	}
+
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.labeling.impl.LabelImplFactory#create()}.
+	 */
+	@Test
+	public void testCreate() {
+		assertNotNull(this.factory.create());
+		assertNotNull(this.factory.create().getLabels());
+		assertTrue(this.factory.create().getLabels().isEmpty());
+	}
+
+	
+	
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.labeling.impl.LabelImplFactory#create(java.util.Set)}
+	 * .
+	 */
+	@Test
+	public void testCreateSetOfPropositions() {
+		assertNotNull(this.factory.create(labels));
+		assertNotNull(this.factory.create(labels).getLabels());
+		
+		assertFalse(this.factory.create(labels).getLabels().isEmpty());
+		assertTrue(this.factory.create(labels).getLabels()!=labels);
+		assertEquals(this.factory.create(labels).getLabels(), labels);
+	}
+	
+	/**
+	 * Test method for
+	 * {@link it.polimi.automata.labeling.impl.LabelImplFactory#create(null)}
+	 * .
+	 */
+	@Test(expected=NullPointerException.class)
+	public void testCreateSetOfPropositionsNUll() {
+		this.factory.create(null);
+	}
+
+}
