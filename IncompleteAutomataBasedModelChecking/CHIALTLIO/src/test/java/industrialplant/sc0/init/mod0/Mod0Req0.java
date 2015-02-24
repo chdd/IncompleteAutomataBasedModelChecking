6057cc25e75af0e65725f8dfd1d293f53923550d
claudiomenghi <cla.menghi@gmail.com>
2015-02-24 14:26:14 +0100

    LTL tests added to CHIA

---
+++ IncompleteAutomataBasedModelChecking/CHIALTLIO/src/test/java/industrialplant/sc0/init/mod0/Mod0Req0.java
@@ -0,0 +1,32 @@
+package industrialplant.sc0.init.mod0;
+
+import industrialplant.Constants;
+import it.polimi.automata.state.State;
+import it.polimi.automata.state.impl.StateFactoryImpl;
+import it.polimi.automata.transition.Transition;
+import it.polimi.automata.transition.impl.TransitionFactoryClaimImpl;
+import it.polimi.automata.transition.impl.TransitionImpl;
+import it.polimi.model.ltltoba.LTLtoBATransformer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class Mod0Req0 {
+
+	
+	@Test
+	public void test() throws IOException {
+		BufferedReader reader=new BufferedReader(new FileReader(getClass().getClassLoader()
+				.getResource(Constants.req0).getFile()));
+		String ltlFormula=reader.readLine();
+		reader.close();
+		LTLtoBATransformer<State, Transition> ltlToBaTransformer;
+		ltlToBaTransformer=new LTLtoBATransformer<State, Transition>(new StateFactoryImpl(), 
+				new TransitionFactoryClaimImpl<State>(TransitionImpl.class));
+		ltlToBaTransformer.transform(ltlFormula);
+	}
+
+}
