14161b0dd3dae768e857d429e0fb43bfaae74160
claudiomenghi <cla.menghi@gmail.com>
2015-02-24 14:27:11 +0100

    LTL constants added

---
+++ IncompleteAutomataBasedModelChecking/CHIALTLIO/src/test/java/industrialplant/Constants.java
@@ -0,0 +1,9 @@
+package industrialplant;
+
+public final class Constants {
+
+	public static final String sc0Path="industrialplant/sc0/";
+	public static final String initPath="init/";
+	public static final String sc0initMod0path=sc0Path+initPath+"mod0/Mod0.xml";
+	public static final String req0=sc0Path+initPath+"mod0/Req0.txt";
+}
