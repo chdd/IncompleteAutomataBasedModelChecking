9e0c0db30a21cd6cc457017eb50780b2ba280086
claudiomenghi <cla.menghi@gmail.com>
2015-08-27 15:20:30 +0200

    CHIAConstraint module added

---
+++ IncompleteAutomataBasedModelChecking/CHIAConstraint/src/main/java/it/polimi/constraints/reachability/ReachabilityRelation.java
@@ -0,0 +1,76 @@
+package it.polimi.constraints.reachability;
+
+import it.polimi.automata.state.State;
+import it.polimi.constraints.transitions.ColoredPluggingTransition;
+
+import java.util.Collection;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+/**
+ * contains the reachability relation. The reachability relation specified
+ * whether an incoming port of a sub-property is reachable from the outgoing
+ * port of the same transition
+ * 
+ * @author Claudio Menghi
+ *
+ */
+public class ReachabilityRelation {
+
+	@Override
+	public String toString() {
+		return "ReachabilityRelation [acceptingMap=" + acceptingMap + "]";
+	}
+	/**
+	 * is the map that specifies for each incoming transition the corresponding
+	 * outgoing transition
+	 */
+
+	private final Multimap<State, ReachabilityEntry> acceptingMap;
+	
+	
+	/**
+	 * creates a new empty reachability relation. The reachability relation is
+	 * used to map the incoming transitions which are reachable from the
+	 * outgoing transition of the same component
+	 */
+	public ReachabilityRelation() {
+		this.acceptingMap = HashMultimap.create();
+	}
+
+	/**
+	 * adds the transition to the reachability relation. The sourceTransition is
+	 * the outgoingPort of the sub-property from which the destinationTransition
+	 * can be reached. The destinationTransition is the incoming port of the
+	 * subproperty which is reachable from the sourceTransition
+	 * 
+	 * @param sourceTransition
+	 *            is the source of the transition
+	 * @param destinationTransition
+	 *            is the destination of the transition
+	 * @throws NullPointerException
+	 *             if one of the two transitions is null
+	 */
+	public void addTransition(ColoredPluggingTransition outgoingTransition,
+			ColoredPluggingTransition incomingTransition, Boolean modelAccepting, Boolean claimAccepting) {
+		Preconditions.checkNotNull(outgoingTransition,
+				"The outgoing transition cannot be null");
+		Preconditions.checkNotNull(incomingTransition,
+				"The destination transition cannot be null");
+		Preconditions.checkArgument(!outgoingTransition.isIncoming(), "The source of the reachability relation must be an outgoingTransition");
+		Preconditions.checkArgument(incomingTransition.isIncoming(), "The destination of the reachability relation must be an incomingTransition");
+		
+		ReachabilityEntry reachabilityEntry=new ReachabilityEntry(incomingTransition, outgoingTransition,  modelAccepting, claimAccepting);
+		this.acceptingMap.put(outgoingTransition.getSource(), reachabilityEntry);
+	}
+
+	
+	public Multimap<State, ReachabilityEntry> getReachabilityAcceptingMap(){
+		return this.acceptingMap;
+	}
+	public Collection<ReachabilityEntry> get(State subPropertyState){
+		return this.acceptingMap.get(subPropertyState);
+	}
+}
---
+++ IncompleteAutomataBasedModelChecking/CHIAConstraint/src/main/java/it/polimi/constraints/transitions/ColoredPluggingTransition.java
@@ -0,0 +1,126 @@
+package it.polimi.constraints.transitions;
+
+import it.polimi.automata.state.State;
+import it.polimi.automata.transition.Transition;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * The Port class is used to describe how the IBA/BA that refers to the
+ * sub-property/replacement to the a transparent state of the model is connected
+ * with the states of the original model. The port class stores in the source,
+ * destination and transition attributes, the source and the destinations state
+ * of the port, and the corresponding transition. Depending on whether the port
+ * represents an in-coming or an out-coming transition the source or the
+ * destination state corresponds with a state of the model. The port is also
+ * associated with a color which specifies whether from the port is possible to
+ * reach an accepting state or is reached from an initial state.
+ * 
+ * The port class contains the source and the destinations state of the port, a
+ * boolean flag that specifies if the port is in-coming or out-coming and the
+ * corresponding transition
+ * 
+ * @author claudiomenghi
+ *
+ */
+public class ColoredPluggingTransition extends PluggingTransition {
+
+	/**
+	 * Is the counter which is used to associate to each new port a new Id
+	 */
+	public static int ID_COUNTER = 1;
+
+	/**
+	 * The port is also associated with a color which specifies whether from the
+	 * port is possible to reach an accepting state or is reached from an
+	 * initial state.
+	 */
+	private Color color;
+
+	public ColoredPluggingTransition(int id, State source, State destination, Transition transition,
+			boolean incoming, Color color) {
+		super(id, source, destination, transition, incoming);
+		Preconditions.checkNotNull(color,
+				"The color of the transitionc cannot be null");
+
+		this.setColor(color);
+	}
+	/**
+	 * creates a new port
+	 * 
+	 * @param source
+	 *            is the source of the transition of the port it can be a state
+	 *            of the refinement of the model or an already specified state
+	 *            of the model
+	 * @param destination
+	 *            is the destination of the transition of the port it can be a
+	 *            state of the refinement of the model or an already specified
+	 *            state of the model
+	 * @param transition
+	 *            is the transition that connect the source with the destination
+	 * @param component
+	 *            the component to which the port belongs
+	 * @throws NullPointerException
+	 *             if one of the parameters is null
+	 */
+	public ColoredPluggingTransition(State source, State destination, Transition transition,
+			boolean incoming, Color color) {
+		this(ColoredPluggingTransition.ID_COUNTER, source, destination, transition, incoming, color);
+		ColoredPluggingTransition.ID_COUNTER = ColoredPluggingTransition.ID_COUNTER + 1;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Port [source=" + this.getSource() + ", incoming=" + this.isIncoming()
+				+ ", destination=" + this.getDestination() + ", transition=" + this.getTransition()
+				+ ", color=" + color + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ColoredPluggingTransition other = (ColoredPluggingTransition) obj;
+		if (color != other.color)
+			return false;
+		return true;
+	}
+
+
+
+	/**
+	 * returns the color associated with the port
+	 * 
+	 * @return the color of the port
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 * sets the color to the port
+	 * 
+	 * @param color
+	 *            the color to set
+	 * @throws NullPointerException
+	 *             if the color is null
+	 */
+	public void setColor(Color color) {
+		Preconditions.checkNotNull(color,
+				"The color of the port cannot be null");
+		this.color = color;
+	}
+}
