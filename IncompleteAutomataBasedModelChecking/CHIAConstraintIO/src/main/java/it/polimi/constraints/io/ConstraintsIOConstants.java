a35285e33b2ea5a6a5fb5032a153f404372c7322
claudiomenghi <cla.menghi@gmail.com>
2015-08-27 16:11:49 +0200

    CHIAConstraintIO module added

---
+++ IncompleteAutomataBasedModelChecking/CHIAConstraintIO/src/main/java/it/polimi/constraints/io/ConstraintsIOConstants.java
@@ -0,0 +1,15 @@
+package it.polimi.constraints.io;
+
+public class ConstraintsIOConstants {
+	
+	public final static String XML_ELEMENT_PORT_SOURCE_STATE="sourcestate";
+	public final static String XML_ELEMENT_PORT_DESTINATION_STATE="destinationstate";
+	public final static String XML_ELEMENT_PORT_TRANSITION="transition";
+
+	public final static String XML_ATTRIBUTE_TRANSITION_ID="id";
+	public final static String XML_ATTRIBUTE_PROPOSITIONS="propositions";
+	
+	
+	public static final String CONSTRAINT_XSD_PATH = "Constraint.xsd";
+	public static final String REPLACEMENT_XSD_PATH = "Replacement.xsd";
+}
