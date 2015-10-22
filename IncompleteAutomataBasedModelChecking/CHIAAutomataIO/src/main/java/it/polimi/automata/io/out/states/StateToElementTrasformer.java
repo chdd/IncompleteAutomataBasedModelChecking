16edec52358bc90f3d242ab9e9e3d020a4a864d0
claudiomenghi <cla.menghi@gmail.com>
2015-10-22 16:04:23 +0200

    adds the StateToElementTrasformer test

--- IncompleteAutomataBasedModelChecking/CHIAAutomataIO/src/main/java/it/polimi/automata/io/out/states/StateToElementTrasformer.java
+++ IncompleteAutomataBasedModelChecking/CHIAAutomataIO/src/main/java/it/polimi/automata/io/out/states/StateToElementTrasformer.java
@@ -10,16 +10,41 @@ import org.w3c.dom.Element;
 
 import com.google.common.base.Preconditions;
 
-public class StateToElementTrasformer  implements Transformer<State, Element>{
-	
+/**
+ * Transforms a State into the corresponding XML element
+ * 
+ * @author Claudio Menghi
+ *
+ */
+public class StateToElementTrasformer implements Transformer<State, Element> {
+
+	/**
+	 * is the document where the XML element must be added
+	 */
 	protected final Document doc;
 
+	/**
+	 * creates a new state transformer
+	 * 
+	 * @param doc
+	 *            is the document that must be modified by the transformer
+	 * @throws NullPointerException
+	 *             if the document is null
+	 */
 	public StateToElementTrasformer(Document doc) {
 		Preconditions.checkNotNull(doc, "The document cannot be null");
 
 		this.doc = doc;
 	}
 
+	/**
+	 * transforms the state into the corresponding element
+	 * 
+	 * @param input
+	 *            is the state to be transformed
+	 * @throws NullPointerException
+	 *             if the state to be transformed is null
+	 */
 	@Override
 	public Element transform(State input) {
 		Preconditions.checkNotNull(input, "The input state cannot be null");
