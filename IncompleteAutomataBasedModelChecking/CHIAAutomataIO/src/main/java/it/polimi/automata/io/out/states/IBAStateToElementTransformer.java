e41e7b79260912ea808447c7a51778e3460eb5cb
claudiomenghi <cla.menghi@gmail.com>
2015-10-23 14:46:06 +0200

    adds the IBAStateToElementTransformerTest

--- IncompleteAutomataBasedModelChecking/CHIAAutomataIO/src/main/java/it/polimi/automata/io/out/states/IBAStateToElementTransformer.java
+++ IncompleteAutomataBasedModelChecking/CHIAAutomataIO/src/main/java/it/polimi/automata/io/out/states/IBAStateToElementTransformer.java
@@ -10,12 +10,38 @@ import org.w3c.dom.Element;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * Transforms a State of an IBA into the corresponding XML element
+ * 
+ * @author Claudio Menghi
+ *
+ */
 public class IBAStateToElementTransformer extends BAStateToElementTransformer {
 
+	/**
+	 * creates a new state transformer
+	 * 
+	 * @param automaton
+	 *            is the Buchi automaton containing the state
+	 * @param doc
+	 *            is the document that must be modified by the transformer
+	 * @throws NullPointerException
+	 *             if the document or the BA is null
+	 */
 	public IBAStateToElementTransformer(IBA automaton, Document doc) {
 		super(automaton, doc);
 	}
 
+	/**
+	 * transforms the state into the corresponding element
+	 * 
+	 * @param input
+	 *            is the state to be transformed
+	 * @throws NullPointerException
+	 *             if the state to be transformed is null
+	 * @throws IllegalArgumentException
+	 *             if the state is not a state of the IBA
+	 */
 	@Override
 	public Element transform(State input) {
 		Preconditions.checkNotNull(input, "The input state cannot be null");
